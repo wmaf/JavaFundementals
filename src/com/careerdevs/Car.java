aa2cf61590de927692046e7e1c89c1f846f31d5a
wmaf <wmaf@outlook.com>
2022-01-11 19:41:34 -0500

    aded F for Float

--- src/com/careerdevs/Car.java
+++ src/com/careerdevs/Car.java
@@ -5,15 +5,16 @@ public class Car {
     public String model;
     public int mileage;
     public double gasTankPercent;
-//public Object [] carArr = {};
-    public Car (String make, String model, int mileage, double gasTankPercent) {
+
+    //public Object [] carArr = {};
+    public Car(String make, String model, int mileage, float gasTankPercent) {
         this.make = make;
         this.model = model;
         this.mileage = mileage;
         this.gasTankPercent = gasTankPercent;
 
-
-
-
+    }
+    public String getCar () {
+        return make;
     }
 }
--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -3,17 +3,17 @@ package com.careerdevs;
 public class Main {
 
     public static void main(String[] args) {
-//       System.out.println("Hello");
-//       Constructor myConstructor = new Constructor("Bill", 60);
-//       System.out.println(myConstructor.name);
+    System.out.println("Hello");
+    Constructor myConstructor = new Constructor("Bill", 45);
+    System.out.println(myConstructor.name);
 
+        Car myCars1 = new Car("Chevy", "Camaro 2SS", 10000, 0.10F);
+        Car myCars2 = new Car("Ford", " Mustang GT", 30000, 0.50F);
+        Car myCars3 = new Car("Dodge", "Hellcat", 100, 0.90F);
+        System.out.println(myCars1.make + " " + myCars1.model + " " + myCars1.mileage + " " + myCars1.gasTankPercent);
+        System.out.println(myCars2.make + " " + myCars2.model + " " + myCars2.mileage + " " + myCars2.gasTankPercent);
+        System.out.println(myCars3.make + " " + myCars3.model + " " + myCars3.mileage + " " + myCars3.gasTankPercent);
 
-    Car myCars1 = new Car("Chevy", "Camaro 2SS",   10000, 0.10);
-    Car myCars2 = new Car("Ford"," Mustang GT", 30000, 0.50);
-    Car myCars3 = new Car("Dodge","Hellcat", 100, 0.90);
-        System.out.println(myCars1.make +" "+ myCars1.model +" "+ myCars1.mileage +" "+ myCars1.gasTankPercent);
-        System.out.println(myCars2.make +" "+ myCars2.model +" "+ myCars2.mileage +" "+ myCars2.gasTankPercent);
-        System.out.println(myCars3.make +" "+ myCars3.model +" "+ myCars3.mileage +" "+ myCars3.gasTankPercent);
     }
 
 }
