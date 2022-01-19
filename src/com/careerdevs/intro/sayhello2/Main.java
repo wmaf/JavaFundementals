8f25af02fafc48084b39bd2b1a2e63d760a086ca
wmaf <wmaf@outlook.com>
2022-01-18 20:54:35 -0500

    corrected file str.

--- src/com/careerdevs/intro/sayhello2/Main.java
+++ src/com/careerdevs/intro/sayhello2/Main.java
@@ -3,9 +3,10 @@ package com.careerdevs.intro.sayhello2;
 import java.util.Scanner;  // Imports the Scanner class
 
 public class Main {
-    //Build the scanner
-    Scanner myScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        //Build the scanner
+        Scanner myScanner = new Scanner(System.in);
         //Ask the question
         System.out.println("Please enter your name:");
         // Get the input
