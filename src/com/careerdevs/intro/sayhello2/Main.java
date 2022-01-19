58d5e6b0c6056099e6bc7ebc7055254c29c207df
wmaf <wmaf@outlook.com>
2022-01-18 21:07:55 -0500

    part B

--- src/com/careerdevs/intro/sayhello2/Main.java
+++ src/com/careerdevs/intro/sayhello2/Main.java
@@ -1,18 +1,23 @@
 package com.careerdevs.intro.sayhello2;
 
+
+
 import java.util.Scanner;  // Imports the Scanner class
 
 public class Main {
-
+    //don't need strings and args as we are accepting input.
     public static void main(String[] args) {
         //Build the scanner
         Scanner myScanner = new Scanner(System.in);
         //Ask the question
-        System.out.println("Please enter your name:");
+        System.out.println("Please enter your first name:");
         // Get the input
-        String name = myScanner.nextLine();
+        String fName = myScanner.nextLine();
+        System.out.println("Please enter your last name:");
+        String lName = myScanner.nextLine();
         //Show it to the user
-        System.out.println("Hello: " + name);
+        System.out.println("Hello: " + fName +" "+ lName);
 
     }
 }
+
